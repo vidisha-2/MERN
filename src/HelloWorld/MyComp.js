6af37c6ee31ac82772f7e7924c914fc1eec3100f
Vidisha <vidu1987@gmail.com>
2020-02-17 18:55:11 -0800

    React with JSX

---
+++ src/HelloWorld/MyComp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class MyComp extends Component {
+    render(){
+        return(
+            <h1>
+                "Hello World"!
+            </h1>
+        );
+    }
+}
+
+export default MyComp;
